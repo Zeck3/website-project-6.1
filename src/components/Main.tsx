bc1063983f80b29c24c7d085a1c593ea8f4fe1f2
Zeck3 <owmsowmsowms@gmail.com>
2026-06-15 19:05:48 +0800

    removing console.logs

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -69,10 +69,8 @@ export default function Main(props: MainProps) {
       const lyrics = parseVtt(vtt);
 
       const update = () => {
-        console.log("timeupdate firing", dom.player?.currentTime);
         const time = dom.player!.currentTime;
         const index = syncLyric(lyrics, time);
-        console.log("sync index:", index);
 
         dom.lyric!.textContent = index == null ? "" : lyrics[index].text;
       };
@@ -191,7 +189,6 @@ const parseVtt = (vtt: string): Subtitle[] => {
 };
 
 const syncLyric = (lyrics: Subtitle[], time: number): number | null => {
-  console.log("parsed lyrics:", lyrics.length);
   const validLyrics = lyrics.filter(
     (lyric) => time >= lyric.startTime && time <= lyric.endTime,
   );
